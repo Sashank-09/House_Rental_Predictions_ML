0effc1b846ba17b667fc0daf13abdfaae00f2218
Sashank <inukurthisasi@gmail.com>
2026-01-23 22:17:39 +0530

    Update Flask app to use environment variables for host and port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
+import os
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
@@ -26,4 +27,8 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
